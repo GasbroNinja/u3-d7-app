d377c8d0c5b07bd4dd6192d0d7bfdb469e9fe214
GasbroNinja <gasbroider@live.it>
2023-04-04 17:31:13 +0200

    problems solved

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,35 +1,24 @@
 import { Card } from "react-bootstrap";
 
 const SingleBook = (props) => {
-  // ** codice precedente alla nuova implementazione: **
-  // state = {
-  //   selected: false
-  // };
-
-
-
-    return (
-      <>
-        <Card
-          onClick={() => props.changeSelectedBook(props.book.asin)}
-
-          style={{
-            border:
-              setSelectedBook === props.book.asin
-                ? "3px solid red"
-                : "3px solid #ebebeb",
-          }}
-        >
-          <Card.Img variant="top" src={props.book.img} />
-          <Card.Body>
-            <Card.Title style={{ color: "black" }}>
-              {props.book.title}
-            </Card.Title>
-          </Card.Body>
-        </Card>
-      </>
-    );
-  }
-
+  return (
+    <>
+      <Card
+        onClick={() => props.changeSelectedBook(props.book.asin)}
+        style={{
+          border:
+            props.selectedBook === props.book.asin
+              ? "3px solid red"
+              : "3px solid #ebebeb",
+        }}
+      >
+        <Card.Img variant="top" src={props.book.img} />
+        <Card.Body>
+          <Card.Title style={{ color: "black" }}>{props.book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
 export default SingleBook;
